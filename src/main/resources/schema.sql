c2fb2552afa4f629cbde333664ed9cdade22c294
bugdebugging <annyousung@gmail.com>
2022-01-07 17:35:39 +0900

    project initialize

---
+++ src/main/resources/schema.sql
@@ -0,0 +1,50 @@
+CREATE TABLE users
+(
+    id         bigint                             NOT NULL AUTO_INCREMENT PRIMARY KEY,
+    username   varchar(30)                        NOT NULL,
+    password   varchar(260)                       NOT NULL,
+    phone      varchar(20)                        NOT NULL,
+    name       varchar(30)                        NOT NULL,
+    created_at datetime default CURRENT_TIMESTAMP NOT NULL,
+    updated_at datetime default CURRENT_TIMESTAMP NOT NULL
+);
+
+CREATE TABLE products
+(
+    id          bigint                             NOT NULL AUTO_INCREMENT PRIMARY KEY,
+    user_id     bigint                             NOT NULL,
+    name        varchar(50)                        NOT NULL,
+    price       bigint                             NOT NULL,
+    description varchar(2048)                      NOT NULL,
+    status      varchar(20)                        NOT NULL,
+    interest    int      default 0                 NOT NULL,
+    created_at  datetime default CURRENT_TIMESTAMP NOT NULL,
+    updated_at  datetime default CURRENT_TIMESTAMP NOT NULL,
+    FOREIGN KEY (user_id) REFERENCES users(id)
+);
+
+CREATE TABLE replies
+(
+    id         bigint                             NOT NULL AUTO_INCREMENT PRIMARY KEY,
+    user_id    bigint                             NOT NULL,
+    product_id bigint                             NOT NULL,
+    comments   varchar(1024)                      NOT NULL,
+    created_at datetime default CURRENT_TIMESTAMP NOT NULL,
+    updated_at datetime default CURRENT_TIMESTAMP NOT NULL,
+    FOREIGN KEY (user_id) REFERENCES users(id),
+    FOREIGN KEY (product_id) REFERENCES products(id)
+);
+
+CREATE TABLE user_images
+(
+    user_id   bigint       NOT NULL,
+    image_url varchar(100) NOT NULL,
+    FOREIGN KEY (user_id) REFERENCES users(id)
+);
+
+CREATE TABLE product_images
+(
+    product_id bigint       NOT NULL,
+    image_url  varchar(100) NOT NULL,
+    FOREIGN KEY (product_id) REFERENCES products(id)
+);
