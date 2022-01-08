22a9a00f13fd90c0f6ce03365870835dac8ae525
bugdebugging <annyousung@gmail.com>
2022-01-08 14:39:10 +0900

    test:제품 이미지포함 등록테스트

--- src/test/java/com/danggeun/market/product/ProductRepositoryTest.java
+++ src/test/java/com/danggeun/market/product/ProductRepositoryTest.java
@@ -1,15 +1,15 @@
 package com.danggeun.market.product;
 
-import com.danggeun.market.product.domain.Money;
-import com.danggeun.market.product.domain.Product;
-import com.danggeun.market.product.domain.ProductRepository;
-import com.danggeun.market.product.domain.ProductStatus;
+import com.danggeun.market.product.domain.*;
 import com.danggeun.market.user.domain.User;
 import com.danggeun.market.user.domain.UserRepository;
 import org.junit.jupiter.api.Test;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.test.context.SpringBootTest;
 import org.springframework.transaction.annotation.Transactional;
+
+import java.util.ArrayList;
+import java.util.List;
 
 import static org.junit.jupiter.api.Assertions.*;
 
@@ -27,9 +27,15 @@ public class ProductRepositoryTest {
         final String name = "조던 신발";
         final Money price = Money.of(700000L);
         final String description = "한번도 안신은 조던신발";
+        final int countOfImages = 3;
+
+        List<ProductImage> uploadedProductImages = new ArrayList<>();
+        for (int i = 0; i < countOfImages; i++) {
+            uploadedProductImages.add(ProductImage.of("https://s3.ap-northeast-2.amazonaws.com/danggeun_product_imgs" + i));
+        }
 
         User user = userRepository.findById(1L).get();
-        Product product = new Product(user, name, price, description);
+        Product product = new Product(user, name, price, description, uploadedProductImages);
         Product savedProduct = productRepository.save(product);
 
         assertNotNull(savedProduct.getId());
@@ -38,5 +44,8 @@ public class ProductRepositoryTest {
         assertEquals(description, savedProduct.getDescription());
         assertEquals(user.getId(), savedProduct.getSeller().getId());
         assertEquals(ProductStatus.SELL, savedProduct.getStatus());
+        for (int i = 0; i < countOfImages; i++) {
+            assertEquals(uploadedProductImages.get(i), product.getProductImages().get(i));
+        }
     }
 }
