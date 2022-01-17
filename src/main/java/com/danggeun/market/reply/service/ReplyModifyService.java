4c566849fe2e60caa7ab0817dd52b460e0721016
bugdebugging <annyousung@gmail.com>
2022-01-17 16:47:22 +0900

    feat:댓글 수정 기능 추가

--- src/main/java/com/danggeun/market/reply/service/ReplyModifyService.java
+++ src/main/java/com/danggeun/market/reply/service/ReplyModifyService.java
@@ -1,6 +1,9 @@
 package com.danggeun.market.reply.service;
 
+import com.danggeun.market.reply.domain.Reply;
 import com.danggeun.market.reply.domain.ReplyRepository;
+import com.danggeun.market.reply.dto.ReplyResponse;
+import com.danggeun.market.reply.service.dto.ReplyModifyCommand;
 import lombok.RequiredArgsConstructor;
 import org.springframework.stereotype.Service;
 import org.springframework.transaction.annotation.Transactional;
@@ -10,4 +13,14 @@ import org.springframework.transaction.annotation.Transactional;
 @RequiredArgsConstructor
 public class ReplyModifyService {
     private final ReplyRepository replyRepository;
+
+    public ReplyResponse modifyReply(ReplyModifyCommand replyModifyCommand) {
+        Reply reply = replyRepository.findByIdWithWriter(replyModifyCommand.getReplyId())
+                .orElseThrow(() -> {
+                    throw new IllegalArgumentException("해당 id의 댓글이 존재하지 않습니다.");
+                });
+        reply.changeContent(replyModifyCommand.getUserId(), replyModifyCommand.getComment());
+
+        return ReplyResponse.fromEntity(reply);
+    }
 }
