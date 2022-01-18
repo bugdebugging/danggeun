1ccf8826e7291abc05f27d32235b59dc49154424
bugdebugging <annyousung@gmail.com>
2022-01-18 13:48:01 +0900

    feat:관심 상품목록 조회

--- src/main/java/com/danggeun/market/interest/domain/InterestHistoryRepository.java
+++ src/main/java/com/danggeun/market/interest/domain/InterestHistoryRepository.java
@@ -1,6 +1,16 @@
 package com.danggeun.market.interest.domain;
 
+import com.danggeun.market.user.domain.User;
 import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.data.jpa.repository.Query;
+import org.springframework.data.repository.query.Param;
+
+import java.util.List;
 
 public interface InterestHistoryRepository extends JpaRepository<InterestHistory, Long> {
+    @Query("SELECT IH,(SELECT COUNT(R) FROM Reply R WHERE R.productId=IH.product.id)," +
+            "(SELECT COUNT(IH2) FROM InterestHistory IH2 WHERE IH2.product.id=IH.product.id)" +
+            " FROM InterestHistory IH JOIN FETCH IH.product WHERE IH.user = :user")
+    List<Object[]> searchInterestProduct(@Param("user") User user);
+
 }
---
+++ src/main/java/com/danggeun/market/interest/service/InterestProductSearchService.java
@@ -0,0 +1,37 @@
+package com.danggeun.market.interest.service;
+
+import com.danggeun.market.interest.domain.InterestHistory;
+import com.danggeun.market.interest.domain.InterestHistoryRepository;
+import com.danggeun.market.product.dto.ProductSummaryResponse;
+import com.danggeun.market.user.domain.User;
+import com.danggeun.market.user.domain.UserRepository;
+import lombok.RequiredArgsConstructor;
+import org.springframework.stereotype.Service;
+import org.springframework.transaction.annotation.Transactional;
+
+import java.util.List;
+import java.util.stream.Collectors;
+
+import static com.google.common.base.Preconditions.checkArgument;
+
+@Service
+@RequiredArgsConstructor
+@Transactional(readOnly = true)
+public class InterestProductSearchService {
+    private final InterestHistoryRepository interestHistoryRepository;
+    private final UserRepository userRepository;
+
+    public List<ProductSummaryResponse> searchInterestProduct(Long userId) {
+        checkArgument(userId != null, "사용자의 id는 필수입니다.");
+        User user = userRepository.findById(userId)
+                .orElseThrow(() -> {
+                    throw new IllegalArgumentException("해당 id와 일치하는 사용자가 존재하지 않습니다.");
+                });
+        List<Object[]> result = interestHistoryRepository.searchInterestProduct(user);
+        return result.stream()
+                .map(objects -> new ProductSummaryResponse(((InterestHistory) objects[0]).getProduct(),
+                        (Long) objects[1],
+                        (Long) objects[2]))
+                .collect(Collectors.toList());
+    }
+}
