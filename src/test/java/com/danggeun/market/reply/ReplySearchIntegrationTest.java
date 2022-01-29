c0829a8f7a68c0aec2c12256e9f9f0e7ad197911
CodeCommit-at-308987748304 <annyousung@gmail.com>
2022-01-29 17:30:03 +0900

    fix:통합테스트 인증 mockuser추가

--- src/test/java/com/danggeun/market/reply/ReplySearchIntegrationTest.java
+++ src/test/java/com/danggeun/market/reply/ReplySearchIntegrationTest.java
@@ -4,6 +4,8 @@ import org.junit.jupiter.api.Test;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.test.autoconfigure.web.servlet.AutoConfigureMockMvc;
 import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.security.test.context.support.WithMockUser;
+import org.springframework.test.context.ActiveProfiles;
 import org.springframework.test.web.servlet.MockMvc;
 import org.springframework.test.web.servlet.request.MockMvcRequestBuilders;
 
@@ -11,6 +13,7 @@ import static org.springframework.test.web.servlet.result.MockMvcResultMatchers.
 import static org.springframework.test.web.servlet.result.MockMvcResultMatchers.status;
 
 @SpringBootTest
+@WithMockUser
 @AutoConfigureMockMvc
 public class ReplySearchIntegrationTest {
     @Autowired
