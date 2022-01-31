c628c829df765fb0aa9dcfe30180e9605ced1484
CodeCommit-at-308987748304 <annyousung@gmail.com>
2022-01-31 17:10:17 +0900

    chore:terraform eks vpc 생성

---
+++ terraform/vpc/main.tf
@@ -0,0 +1,26 @@
+module "vpc" {
+  source = "terraform-aws-modules/vpc/aws"
+
+  name = var.name
+  cidr = var.cidr
+
+  azs = var.azs
+  private_subnets = var.private_subnets
+  public_subnets = var.public_subnets
+
+  enable_nat_gateway = true
+  enable_dns_hostnames = true
+  single_nat_gateway = true
+
+  tags = {
+    "kubernetes.io/cluster/${}" = "shared"
+  }
+  public_subnet_tags = {
+    "kubernetes.io/cluster/${var.eks-cluster-name}" = "shared"
+    "kubernetes.io/role/elb" = "1"
+  }
+  private_subnet_tags = {
+    "kubernetes.io/cluster/${var.eks-cluster-name}" = "shared"
+    "kubernetes.io/role/internal-elb" = "1"
+  }
+}
---
+++ terraform/vpc/variables.tf
@@ -0,0 +1,25 @@
+variable "region" {
+  default = "ap-northeast-2"
+}
+variable "cidr" {
+  description = "The cidr block of vpc"
+  default = "10.0.0.0/16"
+}
+variable "azs" {
+  description = "The available zone in vpc."
+  default = ["ap-northeast-2a","ap-northeast-2b","ap-northeast-2c"]
+}
+variable "private_subnets" {
+  description = "The private subnets of vpc."
+  default = ["10.0.1.0/24", "10.0.2.0/24"]
+}
+variable "public_subnets" {
+  description = "The private subnets of vpc."
+  default = ["10.0.101.0/24", "10.0.102.0/24"]
+}
+variable "name" {
+  default = "danggeun-vpc"
+}
+variable "eks-cluster-name" {
+  default = "danggeun-eks-cluster"
+}
