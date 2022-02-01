c2044fbf2ac19a8a577421a946b05407dfb3fcde
CodeCommit-at-308987748304 <annyousung@gmail.com>
2022-02-01 13:23:07 +0900

    fix:vpc module output

---
+++ terraform/vpc-variables.tf
@@ -0,0 +1,9 @@
+variable "vpc_name" {
+  default = "danggeun"
+}
+variable "region" {
+  default = "ap-northeast-2"
+}
+variable "eks-cluster-name" {
+  default = "daggeun-eks-cluster"
+}
--- terraform/vpc/output.tf
+++ terraform/vpc/output.tf
@@ -4,3 +4,6 @@ output "private_subnet_ids" {
 output "public_subnet_ids" {
   value = module.vpc.public_subnets
 }
+output "vpc_id" {
+  value =  module.vpc.vpc_id
+}
