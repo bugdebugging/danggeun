85074fbfe65ec6a1ebe7998118bc6091fba3c1c9
CodeCommit-at-308987748304 <annyousung@gmail.com>
2022-02-01 17:36:09 +0900

    fix:duplicate variables solve2

--- terraform/rds/main.tf
+++ terraform/rds/main.tf
@@ -1,13 +1,12 @@
 resource "aws_db_subnet_group" "db_private_subnet_group" {
   subnet_ids = var.db_subnet_group_ids
-  name = "rds_subnet_group"
+  name = "private_subnet_group"
 }
 resource "aws_db_instance" "rds_instance" {
   name = var.name
   engine = var.engine
   engine_version = var.engine_version
   instance_class = var.instance_class
-  vpc_security_group_ids = [var.vpc_id]
   db_subnet_group_name = aws_db_subnet_group.db_private_subnet_group.name
   tags = {
     Name=var.name
--- terraform/variables.tf
+++
@@ -1,9 +0,0 @@
-variable "name" {
-  default = "danggeun"
-}
-variable "region" {
-  default = "ap-northeast-2"
-}
-variable "eks-cluster-name" {
-  default = "daggeun-eks-cluster"
-}
