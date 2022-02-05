d7e828f3df985e73f6ca32b8acf083f4d91e1e26
CodeCommit-at-308987748304 <annyousung@gmail.com>
2022-02-05 16:50:39 +0900

    fix:bastionHost vpc default security group추가

--- terraform/bastion/variables.tf
+++ terraform/bastion/variables.tf
@@ -1,2 +1,3 @@
 variable "public_subnet_id" {}
 variable "vpc_id" {}
+variable "vpc_default_security_group_id" {}
--- terraform/vpc/output.tf
+++ terraform/vpc/output.tf
@@ -7,3 +7,6 @@ output "public_subnet_ids" {
 output "vpc_id" {
   value =  module.vpc.vpc_id
 }
+output "vpc_default_security_group_id" {
+  value = module.vpc.default_security_group_id
+}
