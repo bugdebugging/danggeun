5565ae9ca35d4605a519e27eb301ea1fcc8c35ff
CodeCommit-at-308987748304 <annyousung@gmail.com>
2022-02-07 15:03:22 +0900

    fix:sg 뺌

--- terraform/rds/main.tf
+++ terraform/rds/main.tf
@@ -2,12 +2,26 @@ resource "aws_db_subnet_group" "db_private_subnet_group" {
   subnet_ids = var.db_subnet_group_ids
   name = "private_subnet_group"
 }
-resource "aws_db_security_group" "enable_db_security_group" {
+resource "aws_security_group" "rds_security_group" {
   name = "db_security_group"
-  ingress {
-    cidr = var.vpc_cidr
-  }
 }
+resource "aws_security_group_rule" "rds_security_group_ingress_rule" {
+  from_port = 3306
+  protocol = "tcp"
+  security_group_id = aws_security_group.rds_security_group.id
+  to_port = 3306
+  type = "ingress"
+  cidr_blocks = [var.vpc_cidr]
+}
+resource "aws_security_group_rule" "rds_security_group_egress_rule" {
+  from_port = 0
+  protocol = "-1"
+  security_group_id = aws_security_group.rds_security_group.id
+  to_port = 0
+  type = "egress"
+  cidr_blocks = ["0.0.0.0/0"]
+}
+
 resource "aws_db_instance" "rds_instance" {
   name = var.name
   engine = var.engine
@@ -21,5 +35,4 @@ resource "aws_db_instance" "rds_instance" {
   skip_final_snapshot = true
   username = var.db_user
   password = var.db_password
-  security_group_names = [aws_db_security_group.enable_db_security_group.name]
 }
--- terraform/rds/variables.tf
+++ terraform/rds/variables.tf
@@ -13,4 +13,3 @@ variable "engine_version" {
 variable "allocated_storage" {}
 variable "db_user" {}
 variable "db_password" {}
-variable "vpc_cidr" {}
